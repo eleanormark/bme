3a4974353546e3414eab7e29ccddcf46c0dc5220
Eleanor Mark <emark@Eleanors-MBP.attlocal.net>
2017-04-18 14:54:45 -0700

    initial commit

---
+++ js/scrolling-nav.js
@@ -0,0 +1,29 @@
+//jQuery to collapse the navbar on scroll
+$(window).scroll(function() {
+    if ($(".navbar").offset().top > 50) {
+        $(".navbar-fixed-top").addClass("top-nav-collapse");
+    } else {
+        $(".navbar-fixed-top").removeClass("top-nav-collapse");
+    }
+});
+
+// Closes the Responsive Menu on Menu Item Click
+$('.navbar-collapse ul li a').click(function(){ 
+        $('.navbar-toggle:visible').click();
+});
+
+function myMap()
+{
+  myCenter=new google.maps.LatLng(37.773972, -122.431297);
+  var mapOptions= {
+    center:myCenter,
+    zoom:12, scrollwheel: false, draggable: false,
+    mapTypeId:google.maps.MapTypeId.ROADMAP
+  };
+  var map=new google.maps.Map(document.getElementById("googleMap"),mapOptions);
+
+  var marker = new google.maps.Marker({
+    position: myCenter,
+  });
+  marker.setMap(map);
+}
